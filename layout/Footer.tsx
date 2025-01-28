7478e44b2e923d941cc2cb9ecdcd32bf2c31f5b2
Richard Unterberg <mail@richardunterberg.de>
2025-01-28 23:32:50 +0100

    🎨 change to fixed ref links

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -3,6 +3,7 @@ import { Github, Smile } from "lucide-react"
 import LayoutComponent from "#components/common/LayoutComponent"
 import LinkComponent from "#components/common/LinkComponent"
 import useTone from "#tone/useTone"
+import { externalLinks } from "#lib/links"
 
 const Footer = () => {
   const { isPlaying } = useTone()
@@ -10,14 +11,10 @@ const Footer = () => {
   return (
     <LayoutComponent className="mt-10 mb-5 flex flex-col gap-2 justify-center items-center">
       <Smile size={32} className={`${isPlaying ? "animate-spin" : ""}`} />
-      <LinkComponent href="https://unterberg.dev/" className="flex items-center gap-2">
+      <LinkComponent href={externalLinks.author} className="flex items-center gap-2">
         unterberg.dev
       </LinkComponent>
-      <LinkComponent
-        isMenu
-        href="https://github.com/unterberg-dev/statetrain"
-        className="flex items-center gap-2 text-sm"
-      >
+      <LinkComponent isMenu href={externalLinks.github} className="flex items-center gap-2 text-sm">
         <Github size={12} />
         Source code on GitHub
       </LinkComponent>
